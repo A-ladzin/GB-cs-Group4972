877e1c7b04f9e062fc546285ef75288eb74bd15e
Alexander <a.ladzin1@Gmail.com>
2023-09-04 22:24:27 +0300

    Update

---
+++ Sem11ask0/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int Square(int num){
+    if(num <0) num = -num;
+    int power = 0, result = 0, temp = num;
+
+    while(temp >0){
+        //  odd\even
+        if((temp&1) > 0){
+            // result+= num*(2^power)
+            result += (num <<power); 
+
+        }
+
+        power++;
+        // temp/=2;
+        temp >>=1;
+
+    }
+    return result;
+}
+
+
+
+
+
+string inputNum = Console.ReadLine();
+
+if(inputNum!=null)
+{
+    int num = int.Parse(inputNum);
+    Console.WriteLine(Square(num));
+}
+
+
---
+++ Sem1Tast2/Program.cs
@@ -0,0 +1,16 @@
+﻿
+String? input = Console.ReadLine();
+
+int a = int.Parse(input);
+
+if (a == 1){
+    Console.WriteLine("Monday");
+}
+
+
+
+string dayOfTheWeek = System.Globalization.CultureInfo.GetCultureInfo("en-Us").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(a));
+
+Console.WriteLine(dayOfTheWeek);
+
+
