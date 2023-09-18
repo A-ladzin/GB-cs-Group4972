5e2c132cc29927f21b0f0e0f679ce56ef97274a3
Alexander Ladzin <a.ladzin1@gmail.com>
2023-09-18 20:37:59 +0300

    Pull

--- Sem2/Sem2task14/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿
-
-int number = int.Parse(Console.ReadLine()??"0");
-
-Console.WriteLine(number%7==0&&number%23==0);
--- Sem2/sem2Task12/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿
-
-
-int f = int.Parse(Console.ReadLine()??"0");
-int s = int.Parse(Console.ReadLine()??"0");
-
-Console.WriteLine((s%f==0)?true:(s%f)); 
